6426a8c0dce754f59c59403377d9132791258e86
lesliemarlo <lesliemarlo09@gmail.com>
2024-06-01 08:44:24 -0500

    validaciones

--- app/src/main/java/com/example/semana06/MainActivity.java
+++ app/src/main/java/com/example/semana06/MainActivity.java
@@ -51,6 +51,8 @@ public class MainActivity extends AppCompatActivity {
     Button btnRegistra;
 
     EditText txtTitulo, txtAnio, txtSerie;
+    //SEM 10
+    boolean boolNoExisteLibro = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -100,20 +102,61 @@ public class MainActivity extends AppCompatActivity {
                 Categoria objCategoria = new Categoria();
                 objCategoria.setIdCategoria(Integer.parseInt(idCategoria.trim()));
 
-                Libro objLibro = new Libro();
-                objLibro.setTitulo(titulo);
-                objLibro.setAnio(Integer.parseInt(anio));
-                objLibro.setSerie(serie);
-                objLibro.setPais(objPais);
-                objLibro.setCategoria(objCategoria);
-                objLibro.setFechaRegistro(FunctionUtil.getFechaActualStringDateTime());
-                objLibro.setEstado(1);
 
-                registra(objLibro);
+                //SEM10 - VALIDACIONES
+                //metodo
+                noExisteTitulo(titulo);
+                //--
+                if (!titulo.matches("[\\p{L}\\p{M} ]{3,30}")) {
+                    mensajeAlert("Ingrese el Título hasta 30 caracteres");
+                } else if (!anio.matches("[1-2][0-9]{3}")) {
+                    mensajeAlert("Ingrese el Año de 4 dígitos");
+                } else if (!serie.matches("[A-Z]{3}[0-9]{8}")) {
+                    mensajeAlert("Ingrese la Serie 3 caracteres y 8 números");
+                } else if (!boolNoExisteLibro){
+                    mensajeAlert("El libro " + titulo + " ya existe");
+                } else {
+                    Libro objLibro = new Libro();
+                    objLibro.setTitulo(titulo);
+                    objLibro.setAnio(Integer.parseInt(anio));
+                    objLibro.setSerie(serie);
+                    objLibro.setPais(objPais);
+                    objLibro.setCategoria(objCategoria);
+                    objLibro.setFechaRegistro(FunctionUtil.getFechaActualStringDateTime());
+                    objLibro.setEstado(1);
+
+                    registra(objLibro);
+                }
+
+
             }
         });
 
     }
+
+    //SEM 10 - VALIDACION DE REPETICION
+
+ void noExisteTitulo(String titulo){
+
+        Call<List<Libro>> call = serviceLibro.listaLibroPorTituloIgual(titulo);
+        call.enqueue(new Callback<List<Libro>>() {
+            @Override
+            public void onResponse(Call<List<Libro>> call, Response<List<Libro>> response) {
+                if(response.isSuccessful()){
+                    List<Libro> lstSalida = response.body();
+                    if(lstSalida.isEmpty()){
+                        boolNoExisteLibro = true;
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Libro>> call, Throwable t) {
+
+            }
+        });
+    }
+    //--
 
     void cargaPais(){
         Call<List<Pais>>  call = servicePais.listaTodos();
--- app/src/main/java/com/example/semana06/service/ServiceLibro.java
+++ app/src/main/java/com/example/semana06/service/ServiceLibro.java
@@ -3,13 +3,21 @@ package com.example.semana06.service;
 import com.example.semana06.entity.Libro;
 
 
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface ServiceLibro {
 
     @POST("servicio/libro")
     public Call<Libro> registra(@Body Libro objLibro);
+
+    //SEM10 - VALIDACION DE REPETICION
+    @GET("servicio/libro/porTituloIgual/{titulo}")
+    public  Call<List<Libro>> listaLibroPorTituloIgual(@Path("titulo") String libro);
 
 }
